21f5b8e422b61e5fc3fa6396816090a52e9e3b89
williamreis <william@ufr.edu.br>
2025-06-25 14:14:41 -0400

    fix: invoke class

--- project/src/project/main.py
+++ project/src/project/main.py
@@ -4,7 +4,7 @@ import warnings
 
 from datetime import datetime
 
-from crew import Project
+from crew import CryptoTrendCrew
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 
@@ -24,7 +24,7 @@ def run():
     }
 
     try:
-        Project().crew().kickoff(inputs=inputs)
+        CryptoTrendCrew().crew().kickoff(inputs=inputs)
     except Exception as e:
         raise Exception(f"An error occurred while running the crew: {e}")
 
@@ -38,7 +38,7 @@ def train():
         'current_year': str(datetime.now().year)
     }
     try:
-        Project().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+        CryptoTrendCrew().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
 
     except Exception as e:
         raise Exception(f"An error occurred while training the crew: {e}")
@@ -49,7 +49,7 @@ def replay():
     Replay the crew execution from a specific task.
     """
     try:
-        Project().crew().replay(task_id=sys.argv[1])
+        CryptoTrendCrew().crew().replay(task_id=sys.argv[1])
 
     except Exception as e:
         raise Exception(f"An error occurred while replaying the crew: {e}")
@@ -65,7 +65,7 @@ def test():
     }
 
     try:
-        Project().crew().test(n_iterations=int(sys.argv[1]), eval_llm=sys.argv[2], inputs=inputs)
+        CryptoTrendCrew().crew().test(n_iterations=int(sys.argv[1]), eval_llm=sys.argv[2], inputs=inputs)
 
     except Exception as e:
         raise Exception(f"An error occurred while testing the crew: {e}")
